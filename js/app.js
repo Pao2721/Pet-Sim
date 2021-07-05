08f22c90a6476c23c98d51a173999a63d638f309
Pao2721 <pao2723@gmail.com>
2021-07-04 20:46:10 -0400

    2nd commit

---
+++ js/app.js
@@ -0,0 +1,31 @@
+//Variables------------------Variables---------------------Variables\\
+let happiness, hunger, winStatus
+
+//Constants------------------Constants---------------------Constants\\
+
+
+//Cached Elements------------Cached Elements---------------Cached Elements\\
+const musicBtn = document.getElementById('bgmusic')
+
+//Event Listeners------------Event Listeners---------------Event Listeners\\
+ 
+
+//Functions------------------Functions---------------------Functions\\
+//Initializes the game
+ function init() {
+  winStatus = null
+  happiness = 2
+  hunger = 2
+  render()
+}
+
+ function render() {
+
+}
+ function pause()    {
+
+}
+
+function happyMeter () {
+
+}
