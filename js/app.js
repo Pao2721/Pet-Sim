d62e0a66f88232974731f7512f9683c4dc43269f
Pao2721 <pao2723@gmail.com>
2021-07-09 08:15:52 -0400

    Css touches

--- js/app.js
+++ js/app.js
@@ -1,7 +1,6 @@
 //Variables------------------Variables---------------------Variables\\
 let happiness = 4
 let hunger = 1
-let winStatus = false 
 let petName  = "Don Dotmarco"
 
 //Constants------------------Constants---------------------Constants\\
@@ -14,7 +13,7 @@ const header = document.querySelector("h1")
 const messageEl = document.getElementById("message")
 const restartBtn = document.getElementById("restart")
 const foodBtn = document.getElementById("feed")
-const miniGame = document.getElementById("minigame")
+const Play = document.getElementById("play")
 const Dotta = document.getElementById("dotty")
 
 //Event Listeners------------Event Listeners---------------Event Listeners\\
@@ -22,42 +21,30 @@ const Dotta = document.getElementById("dotty")
  //musicBtn.addEventListener('click')
 
  foodBtn.addEventListener("click",(e) =>  {
-   hunger = hunger + 1, render()
+   hunger = hunger + 1, Init()
 })
-//miniGame.addEventListener("click", (e) => {
-// hunger = hunger -1, minigame()
-// })
-// 
-// 
-/*----------Animation----------*/ 
-
-
-
-
-
-
-
-
-
+ Play.addEventListener("click",(e) => {
+   hunger = hunger - 1,
+   happiness = happiness + 1, 
+   Init()
+   
+ })
 
 /*Functions------------------Functions---------------------Functions*/
 //Initializes the game
-init()
+Init()
 
 
-function init() {
-  winStatus = null
-  
-  
-  render()
-}
 
- function render() {
+
+ function Init() {
   
   if (hunger === 1) {
-    messageEl.innerText = `Don Dotmarco is STARVING!!!`
-    } else if (hunger === 2) {
-      messageEl.innerText = `Don Dotmarco is hungry.`
+    messageEl.innerText = "Don Dotmarco is STARVING!!!"
+    Dotta.style.backgroundColor = "crimson"
+    
+  } else if (hunger === 2) {
+      messageEl.innerText = "Don Dotmarco is hungry."
       Dotta.style.backgroundColor = "orange"
     } else if (hunger === 3) {
       messageEl.innerText = "Don Dotmarco is not hungry right now."
@@ -65,22 +52,37 @@ function init() {
     } else {
      messageEl.innerText = "Don Dotmarco is full and dosen't want to eat."
      Dotta.style.backgroundColor = "blue"
-    }
-  
-    if(hunger === 4 && happiness === 6) {
-      winStatus = true
-      messageEl.innerText = `You've lived to dot another day`
-      header.innerText = `You Won!!`
+    } 
+
+    if (hunger === 1 && happiness === 2) {
+      messageEl.innerText = "Don Dotmarco is STARVING!!!"
+      Dotta.style.backgroundColor = "rgb(223,85,113)"
+      
+    } else if (hunger === 2 && happiness === 3) {
+        messageEl.innerText = "Don Dotmarco is hungry."
+        Dotta.style.backgroundColor = "rgb(228,179,89)"
+      } else if (hunger === 3 && happiness === 4) {
+        messageEl.innerText = "Don Dotmarco is not hungry right now."
+        Dotta.style.backgroundColor = "rgb(156,15,156)"
+      } else if(hunger === 4 && happiness === 5)  {
+       messageEl.innerText = "Don Dotmarco is full and dosen't want to eat."
+       Dotta.style.backgroundColor = "rgb(65,65,204)"
+      } 
+  if(hunger === 4 && happiness === 6) {
+      messageEl.innerText = "You've lived to dot another day."
+      header.innerText = "You Won!!"
     }
 
-    if(hunger === 0) {
-      messageEl.innerText = `You somehow managed to kill a dot. Congratulations???`
-      header.innerText = `(you lost)`
+  if(hunger === 0) {
+      messageEl.innerText = "You somehow managed to kill a dot. Congratulations???"
+      header.innerText = "(you lost)"
+      Dotta.style.width = 0
     }
 
-    if(happiness === 0) {
-      messageEl.innerText = `Don Dotmarco has left the building.`
-       header.innerText = `That's All Folks!!!`
+  if(happiness === 0) {
+      messageEl.innerText = "Don Dotmarco has left the building."
+       header.innerText = "That's All Folks!!!"
+       Dotta.style.width = 0
     }
   
 }
@@ -88,14 +90,10 @@ function init() {
 
 
 function happyMeter () { 
-  if(hunger === 1)  {happiness -= 1}
-  if(hunger === 4)  {happiness += 1}
-  if(miniScore < 3) {happiness -= 2}
-  if(miniScore < 4) {happiness -= 1}
-  if(miniScore > 6) {happiness += 1}
-  if(miniScore > 9) {happiness += 2}
-
-  render()
+  if(hunger === 1)  {happiness = happiness - 1}
+  if(hunger === 4)  {happiness = happiness + 1}
+ 
+  Init()
 }
 
 function reInit () {
